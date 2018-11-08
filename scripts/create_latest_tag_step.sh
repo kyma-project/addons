45b16e09a53e9f8b390ccce1393659aca5e47c13
Jasiak <piotr.jasiak@sap.com>
2018-11-08 09:18:35 +0100

    Add changelog generator

--- scripts/create_latest_tag_step.sh
+++ scripts/create_latest_tag_step.sh
@@ -5,5 +5,5 @@ GIT_URL=$1
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 export GIT_TAG=latest
-git tag $GIT_TAG -a -m "Generated tag from TravisCI"
+git tag $GIT_TAG -f -a -m "Generated tag from TravisCI"
 git push ${GIT_URL} $GIT_TAG -f
